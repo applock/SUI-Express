048898b7ed6ef2eb5cd1d75f1babec611701a59d
Iman <ibiswas@adobe.com>
2021-11-26 23:33:33 +0530

    add

---
+++ mongodb.js
@@ -0,0 +1,16 @@
+const MongoClient = require('mongodb').MongoClient
+var db;
+
+module.exports = {
+    connectToServer: () => {
+        MongoClient.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, }, (error, result) => {
+            if (error) throw error
+            db = result.db('bdo')
+            console.log('Connected to DB via MongoDB 1')
+        });
+    },
+
+    getDb: () => {
+        return db;
+    }
+}
