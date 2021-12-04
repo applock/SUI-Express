93ab81d9f8e5abab6004450c29861ece8822dea3
Iman <ibiswas@adobe.com>
2021-12-04 23:16:37 +0530

    data table

--- routes/datatable.js
+++ routes/datatable.js
@@ -2,23 +2,32 @@ const express = require("express");
 const router = express.Router();
 const request = require("request");
 
-router.get("/table/:from/:to", (req, resp) => {
-  // #swagger.tags = ['Data Tables']
-  // #swagger.path = '/data/table/{from}/{to}'
-  // #swagger.description = 'State-wise data table'
+const fs = require("fs");
+var stateStatistics = fs.readFileSync("./static/stateStatistics.json", "utf8");
+stateStatistics = JSON.parse(stateStatistics);
 
-  request(
-    "https://api-uat.startupindia.gov.in:443/sih/api/noauth/dpiit/services/list/states",
-    { json: true },
-    (err, res, body) => {
-      if (err) {
-        return console.log(err);
-      }
-      console.log(body);
-      console.log(res);
-      resp.send(res.body.data);
+router.get("/stateStatistics/:from/:to", (req, resp) => {
+  // #swagger.tags = ['Data Tables']
+  // #swagger.path = '/data/stateStatistics/{from}/{to}'
+  // #swagger.description = 'State-wise data table'
+  var output = {};
+  output.from = req.params.from;
+  output.to = req.params.to;
+
+  request(process.env.STATES_URL, { json: true }, (err, res, body) => {
+    if (err) {
+      return console.log(err);
     }
-  );
+    console.log(body);
+    console.log(res);
+
+    var apiData = res.body.data;
+    for (var state in apiData) {
+      apiData[state].stateStatistics = stateStatistics;
+    }
+    output.data = apiData;
+    resp.send(output);
+  });
 });
 
 router.get("/startups/:from/:to", (req, resp) => {
