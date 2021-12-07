caa9cfdc7ef33843cb80244b46d6a5707a066250
Iman <ibiswas@adobe.com>
2021-12-07 14:53:14 +0530

    updates

--- routes/staticData.js
+++ routes/staticData.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const moment = require("moment");
 
 const fs = require("fs");
 var districtsMap = fs.readFileSync("./static/districts.json", "utf8");
@@ -19,6 +20,34 @@ router.get("/startupTypes", (req, resp) => {
   // #swagger.path = '/static/startupTypes'
   // #swagger.description = 'Static data of Startup Types'
   resp.send(startupTypes);
+});
+
+router.get("/searchDateRanges", (req, resp) => {
+  // #swagger.tags = ['Static Data']
+  // #swagger.path = '/static/searchDateRanges'
+  // #swagger.description = 'Date ranges for getting count'
+
+  var today = moment().format("YYYY-MM-DD");
+  var oneWeekAgo = moment().subtract(1, "weeks").format("YYYY-MM-DD");
+  var oneMonthAgo = moment().subtract(1, "months").format("YYYY-MM-DD");
+  var threeMonthsAgo = moment().subtract(3, "months").format("YYYY-MM-DD");
+  var sixMonthsAgo = moment().subtract(6, "months").format("YYYY-MM-DD");
+  var nineMonthsAgo = moment().subtract(9, "months").format("YYYY-MM-DD");
+
+  console.log(today);
+  console.log(oneWeekAgo);
+  console.log(oneMonthAgo);
+  console.log(threeMonthsAgo);
+  console.log(sixMonthsAgo);
+  console.log(nineMonthsAgo);
+
+  var dates = [];
+  dates.push({ text: "Last week", from: today, to: oneWeekAgo });
+  dates.push({ text: "Last month", from: today, to: oneMonthAgo });
+  dates.push({ text: "Last 3 months", from: today, to: threeMonthsAgo });
+  dates.push({ text: "Last 6 months", from: today, to: sixMonthsAgo });
+  dates.push({ text: "Last 9 months", from: today, to: nineMonthsAgo });
+  resp.send(dates);
 });
 
 module.exports = router;
