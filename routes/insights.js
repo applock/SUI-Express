c9566b88c59aa4683f21c561229c5dd3559499e2
Iman <ibiswas@adobe.com>
2021-12-20 02:26:05 +0530

    state insights

--- routes/insights.js
+++ routes/insights.js
@@ -12,52 +12,63 @@ router.get("/:geographicalEntity/:entityId/:from/:to", (req, resp) => {
   // #swagger.path = '/insight/{geographicalEntity}/{entityId}/{from}/{to}'
   // #swagger.description = 'Insights'
 
-  var options = {
-    method: "POST",
-    url: process.env.BLANK_FILTER_URL,
-    headers: {
-      authority: "api.startupindia.gov.in",
-      "sec-ch-ua":
-        '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
-      accept: "application/json",
-      lang: "",
-      "sec-ch-ua-mobile": "?0",
-      "user-agent":
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
-      "sec-ch-ua-platform": '"Linux"',
-      "content-type": "application/json",
-      origin: "https://www.startupindia.gov.in",
-      "sec-fetch-site": "same-site",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-dest": "empty",
-      referer: "https://www.startupindia.gov.in/",
-      "accept-language": "en-GB,en-US;q=0.9,en;q=0.8,la;q=0.7",
-    },
-    body: JSON.stringify(query),
-  };
+  var output = {};
+  output.from = req.params.from;
+  output.to = req.params.to;
 
-  request(options, (err, res, body) => {
-    if (err) {
-      return console.log(err);
-    }
-    //console.log(body);
-    var allItems = JSON.parse(body).content;
+  var stateWiseCount = fs.readFileSync("./static/stateWiseCount.json", "utf8");
+  stateWiseCount = JSON.parse(stateWiseCount);
 
-    // add available logos
-    if (allItems) {
-      const l = allItems.length;
-      const lu = process.env.PROFILE_LOGO_URL;
-      for (let i = 0; i < l; i++) {
-        let itm = allItems[i];
-        if (itm.pic) {
-          itm.logo = lu + itm.role + "?fileName=" + itm.pic;
-          allItems[i] = itm;
-        }
+  if (req.params.geographicalEntity == "country") {
+    // India level
+    request(process.env.STATES_URL, { json: true }, (err, res, body) => {
+      if (err) {
+        return console.log(err);
       }
-    }
+      var apiData = res.body.data;
+      for (var state in apiData) {
+        apiData[state].statistics =
+          stateWiseCount[apiData[state].id].statistics;
+      }
+      output.data = apiData;
+      resp.send(output);
+    });
+  } else if (req.params.geographicalEntity == "state") {
+    // State level
+    console.log("Getting insights for State with Id - " + req.params.entityId);
+    var stateDetails = stateWiseCount[req.params.entityId];
 
-    resp.send(allItems);
-  });
+    var indArr = [];
+    for (const industry of stateDetails.industry) {
+      industry.percentage = Math.round(
+        (industry.count / stateDetails.TotalIndustry) * 100
+      );
+      indArr.push(industry);
+    }
+    output.industry = indArr;
+
+    var secArr = [];
+    for (const sector of stateDetails.sector) {
+      sector.percentage = Math.round(
+        (sector.count / stateDetails.TotalSector) * 100
+      );
+      secArr.push(sector);
+    }
+    output.sector = secArr;
+
+    var stgArr = [];
+    for (const stage of stateDetails.stage) {
+      stage.percentage = Math.round(
+        (stage.count / stateDetails.TotalStage) * 100
+      );
+      stgArr.push(stage);
+    }
+    output.stage = stgArr;
+
+    resp.send(output);
+  } else {
+    // City/District level
+  }
 });
 
 module.exports = router;
