b59d8f5bd8c04107411d4dc0b07a5654ca27f8e4
applock <ibsanchar@gmail.com>
2024-03-24 19:06:46 +0530

    updates

--- mongodb.js
+++ mongodb.js
@@ -4,11 +4,12 @@ var db;
 module.exports = {
   connectToServer: () => {
     MongoClient.connect(
-      process.env.DB_URL,
+      process.env.DB_URL_CLOUD,
       { useNewUrlParser: true, useUnifiedTopology: true },
       (error, result) => {
         if (error) throw error;
-        db = result.db("bdo");
+        //db = result.db("bdo");
+        db = result.db(process.env.DB_NAME);
         console.log("Connected to DB via MongoDB");
       }
     );
